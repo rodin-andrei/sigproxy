e1e47d66c6fae625272569fbb84f7c2be1edb656
Oleg Lungu <o.lungu@unifun.com>
2019-08-29 16:59:48 +0300

    Fix sctp controller path.

--- src/main/java/com/unifun/sigproxy/controller/SctpController.java
+++ src/main/java/com/unifun/sigproxy/controller/SctpController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("m3ua")
+@RequestMapping("sctp")
 public class SctpController {
 
     @GetMapping(value = "/get", produces = "application/json")
