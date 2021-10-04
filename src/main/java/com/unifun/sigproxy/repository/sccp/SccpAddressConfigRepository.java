99a577ebaa39d6ff1cca47a965d8a5846de3b0a4
Veaceslav Gargaun <gargaunveceslav@gmail.com>
2021-10-04 11:58:28 +0300

    add Sccp controller

--- src/main/java/com/unifun/sigproxy/repository/sccp/SccpAddressConfigRepository.java
+++ src/main/java/com/unifun/sigproxy/repository/sccp/SccpAddressConfigRepository.java
@@ -3,5 +3,5 @@ package com.unifun.sigproxy.repository.sccp;
 import com.unifun.sigproxy.models.config.sccp.SccpAddressConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SccpAddressConfigRepository extends JpaRepository<SccpAddressConfig, Long> {
+public interface SccpAddressConfigRepository extends JpaRepository<SccpAddressConfig, Integer> {
 }
--- src/main/java/com/unifun/sigproxy/repository/sccp/SccpMtp3DestinationConfigRepository.java
+++ src/main/java/com/unifun/sigproxy/repository/sccp/SccpMtp3DestinationConfigRepository.java
@@ -3,5 +3,5 @@ package com.unifun.sigproxy.repository.sccp;
 import com.unifun.sigproxy.models.config.sccp.SccpMtp3DestinationConfig;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SccpMtp3DestinationConfigRepository extends JpaRepository<SccpMtp3DestinationConfig, Long> {
+public interface SccpMtp3DestinationConfigRepository extends JpaRepository<SccpMtp3DestinationConfig, Integer> {
 }
