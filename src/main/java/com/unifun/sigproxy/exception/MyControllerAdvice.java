1986f5ba7abf03bf4340707292e1e0e29d941604
Veaceslav Gargaun <gargaunveceslav@gmail.com>
2021-10-26 15:47:08 +0300

    fix http status for exceptions

--- src/main/java/com/unifun/sigproxy/exception/MyControllerAdvice.java
+++ src/main/java/com/unifun/sigproxy/exception/MyControllerAdvice.java
@@ -8,14 +8,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Log4j
 @ControllerAdvice
 public class MyControllerAdvice {
 
     @ExceptionHandler(SS7NotContentException.class)
     @ResponseBody
-    public ApiBaseErrorDto handleNoContentEntity(SS7NotContentException ex, HttpServletRequest request){
+    public ApiBaseErrorDto handleNoContentEntity(SS7NotContentException ex, HttpServletRequest request,  HttpServletResponse response){
         log.info(String.valueOf(ex));
+        response.setStatus(204);
         return ApiBaseErrorDto.builder()
                 .patch(request.getRequestURI())
                 .code(HttpStatus.NO_CONTENT.value())
@@ -26,8 +29,9 @@ public class MyControllerAdvice {
 
     @ExceptionHandler(SS7NotFoundException.class)
     @ResponseBody
-    public ApiBaseErrorDto handleNotFoundEntity(SS7NotFoundException ex, HttpServletRequest request){
+    public ApiBaseErrorDto handleNotFoundEntity(SS7NotFoundException ex, HttpServletRequest request,  HttpServletResponse response){
         log.info(String.valueOf(ex));
+        response.setStatus(404);
         return ApiBaseErrorDto.builder()
                 .patch(request.getRequestURI())
                 .code(HttpStatus.NOT_FOUND.value())
@@ -38,11 +42,25 @@ public class MyControllerAdvice {
 
     @ExceptionHandler(SS7AddException.class)
     @ResponseBody
-    public ApiBaseErrorDto handleAddClientLinkException(SS7AddException ex, HttpServletRequest request){
+    public ApiBaseErrorDto handleAddException(SS7AddException ex, HttpServletRequest request, HttpServletResponse response){
         log.info(String.valueOf(ex));
+        response.setStatus(400);
         return ApiBaseErrorDto.builder()
                 .patch(request.getRequestURI())
                 .code(HttpStatus.BAD_REQUEST.value())
+                .type(ex.getClass().getName())
+                .detail(ex.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(SS7RemoveException.class)
+    @ResponseBody
+    public ApiBaseErrorDto handleRemoveException(SS7AddException ex, HttpServletRequest request, HttpServletResponse response){
+        log.info(String.valueOf(ex));
+        response.setStatus(404);
+        return ApiBaseErrorDto.builder()
+                .patch(request.getRequestURI())
+                .code(HttpStatus.NOT_FOUND.value())
                 .type(ex.getClass().getName())
                 .detail(ex.getMessage())
                 .build();
